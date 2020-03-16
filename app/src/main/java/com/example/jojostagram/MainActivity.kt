6865d9cf5764bed7219ac5fd330a61dcdac306c5
JoJoLee <respecticebear@gmail.com>
2020-03-16 21:02:22 +0900

    Add : 메인 화면 하단 네비게이션 바 추가
    메인 화면 하단 네비게이션 바 추가 및 프래그먼트별 구분을 위해 백그라운드 색상 추가
    포토 화면은 추후 화면 구성 후 추가

--- app/src/main/java/com/example/jojostagram/MainActivity.kt
+++ app/src/main/java/com/example/jojostagram/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.jojostagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.jojostagram.navigation.AlarmFragment
+import com.example.jojostagram.navigation.DetailViewFragment
+import com.example.jojostagram.navigation.GridFragment
+import com.example.jojostagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId){
+            R.id.action_home ->{
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search ->{
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo ->{
+
+                return true
+            }
+            R.id.action_favorite_alarm ->{
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account ->{
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
     }
 }
---
+++ app/src/main/java/com/example/jojostagram/navigation/AlarmFragment.kt
@@ -0,0 +1,16 @@
+package com.example.jojostagram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jojostagram.R
+
+class AlarmFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+        return view
+
+    }
+}

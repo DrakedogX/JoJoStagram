b130e8efc41de854988721691e16163bff121858
Joel Lee <respecticebear@gmail.com>
2020-03-21 22:31:04 +0900

    Add: 상세 페이지 화면 추가
    게시글 상세 페이지 관련 화면 및 glide 이미지 라이브러 추가

--- app/src/main/java/com/example/jojostagram/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/jojostagram/navigation/DetailViewFragment.kt
@@ -5,12 +5,61 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.jojostagram.R
+import com.example.jojostagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.squareup.okhttp.OkHttpClient
 
 class DetailViewFragment : Fragment() {
+
+    // Firebase 유저 전역 변수
+    var user: FirebaseUser? = null
+
+    // Firestore 저장소 전역 변수
+    var firestore: FirebaseFirestore? = null
+
+    // Firestore 리스너 등록 전역 변수
+    var imagesSnapshot: ListenerRegistration? = null
+
+    // OkHttpClient 전역 변수
+    var okHttpClient: OkHttpClient? = null
+
+    // 뷰 참조 전역 변수
+    var mainView: View? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
+
+        firestore = FirebaseFirestore.getInstance()
+
         return view
+    }
+
+    // 리사이클러뷰 어댑터
+    inner class DetailRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        val contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+        val contentUidList: ArrayList<String> = arrayListOf()
+
+        // 생성자
+        init{
+            firestore?.collection("images")
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
 
     }
 }

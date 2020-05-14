aaeaf1cd6605c7e7050760770a790469e035e9ec
Joel Lee <respecticebear@gmail.com>
2020-05-14 17:42:16 +0900

    Add: 스플래시 화면 추가위해 액티비티 생성

---
+++ app/src/main/java/com/joel/jojostagram/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.joel.jojostagram.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joel.jojostagram.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
